c4f88f23068de6739adcf582dd97641f95a8a178
Luiz Otávio da Silva Castanheira <luizoscastanheira@hotmail.com.br>
2025-10-27 11:09:17 -0300

    Criadas as páginas Home e Sabores

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,49 @@
-
+import Image from "next/image";
 import estilos from "./page.module.css";
+
+import Sabores from "../../public/banner-sabores.jpg";
+import Eventos from "../../public/eventos-image.jpg";
+import Sobre from "../../public/sobre-image.jpg";
 
 export default function Home() {
   return (
     <main>
-      <h1>Teste Pagina Home</h1>
+      <section className={estilos.sectionBanner}>
+        <h1>SORVETE ARTESANAL</h1>
+      </section>
+
+      <section className={estilos.bannerImagemTexto}>
+        <Image className={estilos.imgBanners} src={Sabores} alt="Imagem de várias bolas e sabores de sorvete." />
+        <div className={estilos.boxTextoBanners}>
+          <h2>NOSSOS SABORES</h2>
+          <span>Novos e deliciosos</span>
+          <p>
+            Sorvete é aquele feito com os melhores ingredientes! Aqui na gelateria todos os nossos produtos são naturais, à base de frutas e sem nenhum conservante! Também temos opções sem lactose e sem açúcar. Venha conhecer e perceber que tem como sorvete ser delicioso e saudável ao mesmo tempo! 
+          </p>
+        </div>
+      </section>
+
+      <section className={estilos.bannerImagemTexto}>
+        <div className={estilos.boxTextoBanners}>
+          <h2>NOSSOS EVENTOS</h2>
+          <span>Delícias de Sorvete</span>
+          <p>
+            Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvetes da sua vida! Venha nos conhecer e passar um tempo aqui com a gente
+          </p>
+        </div>
+        <Image className={estilos.imgBanners} src={Eventos} alt="Imagem de vários potes de sorvete sobre uma mesa de festa." />
+      </section>
+
+      <section className={estilos.bannerImagemTexto}>
+        <Image className={estilos.imgBanners} src={Sobre} alt="Imagem de um griupo de amigos com casquinhas de vários sabores de sorvete." />
+        <div className={estilos.boxTextoBanners}>
+          <h2>SOBRE NÓS</h2>
+          <span>Alegria em cada Casquinha</span>
+          <p>
+            Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há vários anos no mercado produzindo o quem tem de melhor para o nosso cliente e você não pode ficar de fora dessa. Venha nosfazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
--- src/app/sabores/page.js
+++ src/app/sabores/page.js
@@ -1,9 +1,61 @@
+import Image from "next/image";
 import estilos from "./Sabores.module.css"
+
+import Oreo from "../../../public/sabor-oreo.png";
+import Pistache from "../../../public/sabor-pistache.png";
+import Cookies from "../../../public/sabor-cookies-avela.png";
+import Kiwi from "../../../public/sabor-kiwi.png";
+import Morango from "../../../public/sabor-morango.png";
+import Limao from "../../../public/sabor-limao.png";
 
 export default function Sabores(){
     return(
         <div>
-            
+            <section className={estilos.sectionBanner}>
+                <h1>NOSSOS SABORES</h1>
+            </section>
+
+            <section className={estilos.sectionCards}>
+                <h2>SABORES DE SORVETE</h2>
+                <div className={estilos.containerCards}>
+
+                    <div className={estilos.card}>
+                        <Image className={estilos.imgCard} src={Oreo} alt="Imagem de uma bola de sorvete sabor Oreo com um biscoito" />
+                        <h3>Soverte de Oreo</h3>
+                        <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
+                    </div>
+
+                    <div className={estilos.card}>
+                        <Image className={estilos.imgCard} src={Pistache} alt="Imagem de uma bola de sorvete sabor Pistache" />
+                        <h3>Soverte de Pistache</h3>
+                        <p>Delicioso sorvete Pistache com pedacinhos de semente.</p>
+                    </div>
+
+                    <div className={estilos.card}>
+                        <Image className={estilos.imgCard} src={Cookies} alt="Imagem de uma bola de sorvete sabor Cookies e Avalã" />
+                        <h3>Soverte de Cookies e Avelã</h3>
+                        <p>Nosso melhor sorvete. Você vai adorar o sabor.</p>
+                    </div>
+
+                    <div className={estilos.card}>
+                        <Image className={estilos.imgCard} src={Kiwi} alt="Imagem de uma bola de sorvete sabor Kiwi" />
+                        <h3>Soverte de Kiwi</h3>
+                        <p>Delicioso e refrescante sorvete sabor kiwi. Rico em Vitamina C.</p>
+                    </div>
+
+                    <div className={estilos.card}>
+                        <Image className={estilos.imgCard} src={Morango} alt="Imagem de uma bola de sorvete sabor Morango" />
+                        <h3>Soverte de Morango</h3>
+                        <p>Sorvete de Morango gourmet tradicional e saboroso.</p>
+                    </div>
+
+                    <div className={estilos.card}>
+                        <Image className={estilos.imgCard} src={Limao} alt="Imagem de uma bola de sorvete sabor Limão" />
+                        <h3>Soverte de Limão Siciliano</h3>
+                        <p>O incrível sorvete gourmet de limão siciliano vai te encontrar</p>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
